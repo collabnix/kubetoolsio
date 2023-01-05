b51393230bc4392a23156aef3a19bd2926d85b73
Ajeet Singh Raina <ajeetraina@Docker-Ajeet-Singh-Rainas-MacBook-Pro.local>
2023-01-05 14:53:55 +0530

    Added

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/ajeetraina/jan/kubetoolsio/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/ajeetraina/jan/kubetoolsio/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/ajeetraina/jan/kubetoolsio/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/ajeetraina/jan/kubetoolsio/src/css/custom.css'),
+];
